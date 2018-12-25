ead0f6eee266839ea8f05c0ea4f61bbd35546cbf
Yonggang Luo <luoyonggang@gmail.com>
2018-12-25 23:36:16 +0800

    The version may works.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM alpine:edge
+FROM alpine:3.6
 MAINTAINER Seonggi Yang <seonggi.yang@gmail.com>
 
 RUN apk add --no-cache \
     --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
-    pptpclient dante-server
+    pptpclient dante-server curl
+
 ADD entrypoint.sh /entrypoint.sh
+RUN chmod 755 /entrypoint.sh
 ADD sockd.conf /etc/sockd.conf
 EXPOSE 1080
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,33 +5,18 @@ sysctl -w net.ipv6.conf.all.forwarding=1
 
 cat > /etc/ppp/peers/vpn <<_EOF_
 pty "pptp ${SERVER} --nolaunchpppd"
-lock
-noauth
-refuse-pap
-refuse-eap
-refuse-chap
-refuse-mschap
-require-mppe-128
-nobsdcomp
-nodeflate
 name "${USERNAME}"
 password "${PASSWORD}"
-_EOF_
-
-cat > /etc/ppp/ip-up <<_EOF_
-#!/bin/sh
-ip route add 0.0.0.0/1 dev ppp0
-ip route add 128.0.0.0/1 dev ppp0
-_EOF_
-
-cat > /etc/ppp/ip-down <<_EOF_
-#!/bin/sh
-ip route del 0.0.0.0/1 dev ppp0
-ip route del 128.0.0.0/1 dev ppp0
+remotename PPTP
+ipparam vpn
+require-mppe-128
+file /etc/ppp/options.pptp
 _EOF_
 
 # fooo
-route add -net 192.168.0.0 netmask 255.255.0.0 gw 172.17.0.1
+# route add -net 192.168.0.0 netmask 255.255.0.0 gw 172.17.0.1
 
-pon vpn debug dump logfd 2 updetach persist "$@"
-exec sockd
+# pon vpn debug dump logfd 2 updetach persist "$@"
+# ip route replace default dev ppp0
+# exec sockd
+tail -f /dev/null
